f7e08c7155490f86fb91466d69f7fd53ce09c280
wquynguy <wquyenle@gmail.com>
2024-02-07 10:21:15 -0600

    added correct/incorrect answer functionality

--- script.js
+++ script.js
@@ -135,6 +135,10 @@ function showQuestion() {
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 };
 
@@ -145,6 +149,16 @@ function resetState() {
         answerButtons.removeChild(answerButtons.firstChild); // remove all previous answers // 
     }; 
 };
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect){
+        selectedBtn.classList.add("correct");
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+}
 
 startQuiz(); 
 
